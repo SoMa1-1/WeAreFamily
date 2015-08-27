1db702d45cb684812c4d7e2450ef4a48bae5e508
tigersden <expect017@naver.com>
2015-08-27 12:44:04 +0900

    MainActivity 변경

--- app/src/main/java/com/waf/soma/wearefamily/MainActivity.java
+++ app/src/main/java/com/waf/soma/wearefamily/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,18 +19,8 @@ public class MainActivity extends Activity {
 
         SharedPreferences setting = getSharedPreferences("setting", 0);
         SharedPreferences.Editor editor = setting.edit();
-        //TODO 두번째 인자를 true로 설정하면 자동로그인 활성화
         editor.putBoolean("isLogined", true);
         editor.commit();
-
-        WebView webview = (WebView)findViewById(R.id.webview);
-        webview.setWebViewClient(new WebClient()); // 응룡프로그램에서 직접 url 처리
-
-        WebSettings set = webview.getSettings();
-        set.setJavaScriptEnabled(true);
-        set.setBuiltInZoomControls(true);
-        webview.loadUrl("http://www.google.com");
-
 
         Intent myIntent = new Intent(this,MainService.class);
         startService(myIntent);
